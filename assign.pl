69ac7e969bc745cadbcc61b15865177a754bf4af
kushagra-2503 <agrawalkushagra72@gmail.com>
2018-11-30 00:24:28 +0530

    Added length predicate

--- assign.pl
+++ assign.pl
@@ -1,6 +1,8 @@
 contains(X,[X|T]).
 contains(X,[H|T]):- contains(X,T).
 
+len([],0).
+len([H|T],L):- len(T,M), L is M+1.
 
 rule(P1,P2,P3,P4,P5,P6,P7,P8):- data(I,Z,adapter(L1),ether(vid(L2),proto(L3)),ip(src_addr(L4),
 								dst_addr(L5),tcp_udp_src_port(L6),tcp_udp_dest_port(L7),icmp_code(L8)))
@@ -18,5 +20,5 @@ test(Z,I):- Z=accept,string_concat("Packet accepted by rule : ",I,S),write(S).
 
 packet(X):- split_string(X," ,"," ",L),nth0(1,L,P1,_),nth0(4,L,P2,_),nth0(6,L,P3,_),
 			nth0(10,L,P4,_),nth0(13,L,P5,_),nth0(17,L,P6,_),nth0(20,L,P7,_),nth0(23,L,P8,_),
-			rule(P1,P2,P3,P4,P5,P6,P7,P8).
+			rule(P1,P2,P3,P4,P5,P6,P7,P8), len(L,Q), write(Q).
 
