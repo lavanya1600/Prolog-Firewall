e7f05dd4b6f81ae841f4e19759f4fc37bdda718c
Lavanya Soni <sonirishu16@gmail.com>
2018-11-29 17:26:22 +0530

    Second Commit

--- assign.pl
+++ assign.pl
@@ -1,7 +1,17 @@
+contains(X,[X|T]).
+contains(X,[H|T]):- contains(X,T).
 
-rule(X,Y):- data(Z,adapter(L),ip_addr(L2)),contains(X,L),contains(Y,L2), test(Z).
-test(Z):- Z=reject,write("Packet Rejected").
-test(Z):- Z=accept,write("Packet Accepted").
 
-packet(X):- split_string(X," ,"," ",L),nth0(1,L,P1,_),nth0(3,L,P2,_),nth0(5,L,P3,_),
-			nth0(7,L,P4,_),nth0(9,L,P5,_),nth0(11,L,P6,_),nth0(13,L,P7,_),rule(P1,P2,P3,P4,P5,P6,P7).
+rule(P1,P2,P3,P4,P5,P6,P7,P8):- data(I,Z,adapter(L1),ether(vid(L2),proto(L3)),ip(src_addr(L4),
+								dst_addr(L5),tcp_udp_src_port(L6),tcp_udp_dest_port(L7),icmp_code(L8)))
+								,contains(P1,L1),contains(P2,L2),contains(P3,L3),contains(P4,L4),contains(P5,L5),
+								contains(P6,L6),contains(P7,L7),contains(P8,L8),test(Z,I).
+
+test(Z,I):- Z=reject,write("Packet Rejected").
+
+
+test(Z,I):- Z=accept,string_concat("Packet accepted by rule number: ",I,S),write(S).
+
+
+packet(X):- split_string(X," ,"," ",L),nth0(1,L,P1,_),nth0(4,L,P2,_),nth0(6,L,P3,_),
+			nth0(10,L,P4,_),nth0(13,L,P5,_),nth0(17,L,P6,_),nth0(20,L,P7,_),nth0(23,L,P8,_),rule(P1,P2,P3,P4,P5,P6,P7,P8).
--- data.pl
+++ data.pl
@@ -1,10 +1,8 @@
-data(accept,adapter(["A","B","C"]),ether(vid(["1","2"]),proto([X])),
-	ip(src_addr(["192.168.1.1"]),dst_addr([Y]),
-	icmp_code([P]),
-	tcp_udp_src_port(["80"]),tcp_udp_src_dest([Z])).
+data("1",accept,adapter(["A","B","C"]),ether(vid(["1","2"]),proto([X])),
+	ip(src_addr(["192.168.1.1"]),dst_addr(Y),
+	tcp_udp_src_port(["80"]),tcp_udp_dest_port([Z]),icmp_code([P]))).
 
 	/* accept adapter A-C ether vid 1,2 ip src addr 192.168.1.1 tcp src port 80 */
 
-data(reject,adapter(["D","B","F"]),ip_addr(["135","123"])).
-data(reject,adapter([X]),ip_addr([Y])).
+
 
