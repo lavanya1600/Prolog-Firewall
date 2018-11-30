9e5107a2e76a47d417bd4505153fe5c51ba01abe
Lavanya Soni <sonirishu16@gmail.com>
2018-11-30 11:57:07 +0530

    Documented

--- data.pl
+++ data.pl
@@ -1,5 +1,5 @@
 data("1",accept,adapter(["A","B","C"]),ether(vid(["1","2"]),proto([X])),
-	ip(src_addr(["192.168.1.1"]),dst_addr(Y),
+	ip(src_addr(["192.168.1.1"]),dst_addr([Y]),
 	tcp_udp_src_port(["80"]),tcp_udp_dest_port([Z]),icmp_code([P]))).
 
 /* Rejects unmatched packets by default */
