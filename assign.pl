89a46a9939a9daf3c473cb0a9f560541c982446e
kushagra-2503 <agrawalkushagra72@gmail.com>
2018-11-30 22:42:03 +0530

    No change

--- assign.pl
+++ assign.pl
@@ -2,16 +2,17 @@ contains(X,[X|T]).
 contains(X,[H|T]):- contains(X,T).
 
 
-rule(P1,P2,P3,P4,P5,P6,P7,P8):- data(I,Z,adapter(L1),ether(vid(L2),proto(L3)),ip(src_addr(L4),
+check_rule(P1,P2,P3,P4,P5,P6,P7,P8):- data(I,Z,adapter(L1),ether(vid(L2),proto(L3)),ip(src_addr(L4),
 								dst_addr(L5),tcp_udp_src_port(L6),tcp_udp_dest_port(L7),icmp_code(L8)))
-								,contains(P1,L1),contains(P2,L2),contains(P3,L3),contains(P4,L4),contains(P5,L5),
-								contains(P6,L6),contains(P7,L7),contains(P8,L8),test(Z,I).
+								,check_lists([P1,P2,P3,P4,P5,P6,P7,P8], [L1,L2, L3, L4,L5,L6,L7,L8]),print_result(Z,I).
+check_lists([],[]).
+check_lists([H1|T1],[H2|T2]):-contains(H1,H2),check_lists(T1,T2).
 
-test(Z,I):- Z=reject,write("Packet Rejected").
+print_result(Z,I):- Z=reject,write("Packet Rejected").
 
 
-test(Z,I):- Z=accept,string_concat("Packet accepted by rule number: ",I,S),write(S).
+print_result(Z,I):- Z=accept,string_concat("Packet accepted by rule number: ",I,S),write(S).
 
 
 packet(X):- split_string(X," ,"," ",L),nth0(1,L,P1,_),nth0(4,L,P2,_),nth0(6,L,P3,_),
-			nth0(10,L,P4,_),nth0(13,L,P5,_),nth0(17,L,P6,_),nth0(20,L,P7,_),nth0(23,L,P8,_),rule(P1,P2,P3,P4,P5,P6,P7,P8).
+			nth0(10,L,P4,_),nth0(13,L,P5,_),nth0(17,L,P6,_),nth0(20,L,P7,_),nth0(23,L,P8,_),check_rule(P1,P2,P3,P4,P5,P6,P7,P8).
--- data.pl
+++ data.pl
@@ -1,8 +1,3 @@
 data("1",accept,adapter(["A","B","C"]),ether(vid(["1","2"]),proto([X])),
 	ip(src_addr(["192.168.1.1"]),dst_addr(Y),
 	tcp_udp_src_port(["80"]),tcp_udp_dest_port([Z]),icmp_code([P]))).
-
-	/* accept adapter A-C ether vid 1,2 ip src addr 192.168.1.1 tcp src port 80 */
-
-
-
