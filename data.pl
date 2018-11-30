444d0ad30ce0631cc9c5c809790d6193722e7e4f
Lavanya Soni <sonirishu16@gmail.com>
2018-11-30 18:36:54 +0530

    Added top level preedicates

--- data.pl
+++ data.pl
@@ -1,12 +1,38 @@
-data("1",accept,adapter(["A","B","C"]),ether(vid(["1","2"]),proto([X])),
-	ip(src_addr(["192.168.1.1"]),dst_addr([Y]),
-	tcp_udp_src_port(["80"]),tcp_udp_dest_port([Z]),icmp_code([P]))).
+reject("1",
+	adapter(["A,B,C"]),
+	ether(vid(["1","2"]),
+	proto([X])),
+	ip(src_addr(["192.168.1.1"]),
+	dst_addr([Y]),
+	tcp_udp_src_port(["80"]),
+	tcp_udp_dest_port([Z]),
+	icmp_code([P]))).
+
+ accept("2",
+	adapter(["A","B","C"]),
+	ether(vid(["1","2"]),
+	proto([X])),
+	ip(src_addr(["192.168.1.1"]),
+	dst_addr([Y]),
+	tcp_udp_src_port(["80"]),
+	tcp_udp_dest_port([Z]),
+	icmp_code([P]))). 
+
 
 /* Rejects unmatched packets by default */
 
-data("default",reject,adapter(A),ether(vid(B),proto(C)),
-	ip(src_addr(D),dst_addr(E),
-	tcp_udp_src_port(F),tcp_udp_dest_port(G),icmp_code(H))).
+reject("default",
+	adapter(A),
+	ether(vid(B),
+	proto(C)),
+	ip(src_addr(D),
+	dst_addr(E),
+	tcp_udp_src_port(F),
+	tcp_udp_dest_port(G),
+	icmp_code(H))).
+
+
+
 
 /* accept adapter A-C ether vid 1,2 ip src addr 192.168.1.1 tcp src port 80 */
 
